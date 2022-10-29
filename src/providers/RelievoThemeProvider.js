481000e444080c074a5b965eb304a32167a0b7c3
rishbitsbytes <rishrathore1613@gmail.com>
2022-10-30 02:09:32 +0530

    updated: theme settings

--- src/providers/RelievoThemeProvider.js
+++ src/providers/RelievoThemeProvider.js
@@ -1,72 +1,83 @@
-import { createContext, useContext, useState, useMemo } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createContext, useContext, useState, useMemo, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const appThemeContext = createContext({ toggleTheme: () => { }, mode: 'dark' });
+const appThemeContext = createContext({ toggleTheme: () => {}, mode: "dark" });
 
 const AppThemeProvider = ({ children }) => {
+  const initialTheme = localStorage.getItem("relievo-theme") || "dark";
+  const [mode, setMode] = useState(initialTheme);
 
-    const [mode, setMode] = useState('dark');
+  useEffect(() => {
+    localStorage.setItem("relievo-theme", mode);
+  }, [mode]);
 
-    const toggleMode = useMemo(() => ({
-        toggleTheme: () => {
-            setMode((prevMode) => (prevMode === 'dark' ? 'light' : 'dark'));
-        },
-        mode
-    }), [mode]);
+  const toggleMode = useMemo(
+    () => ({
+      toggleTheme: () => {
+        setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
+      },
+      mode,
+    }),
+    [mode]
+  );
 
-    const theme = useMemo(() => createTheme({
+  const theme = useMemo(
+    () =>
+      createTheme({
         palette: {
-            mode,
-            ...(mode === 'light' ?
-                {
-                    // palette values for light mode
-                    primary: {
-                        light: '#f5f5f5',
-                        main: '#ffffff',
-                        dark: '#dee2e5'
-                    },
-                    secondary: {
-                        main: '#6261bd'
-                    },
-                    otherColors: {
-                        white: '#ffffff',
-                        black: '#000000',
-                        action: '#EF4F4F',
-                        gray: '#1a1a24'
-                    },
-                    typography: {
-                        fontFamily: 'Gruppo',
-                    },
-                } :
-                {
-                    // palette values for dark mode
-                    primary: {
-                        light: '#121212',
-                        main: '#212121',
-                        dark: '#ffffff'
-                    },
-                    secondary: {
-                        main: '#6261bd'
-                    },
-                    otherColors: {
-                        white: '#ffffff',
-                        black: '#000000',
-                        action: '#EF4F4F',
-                        gray: '#d4d4d4'
-                    },
-                    typography: {
-                        fontFamily: 'Gruppo',
-                    },
-                }),
+          mode,
+          ...(mode === "light"
+            ? {
+                // palette values for light mode
+                primary: {
+                  light: "#f5f5f5",
+                  main: "#ffffff",
+                  dark: "#dee2e5",
+                },
+                secondary: {
+                  main: "#6261bd",
+                },
+                otherColors: {
+                  white: "#ffffff",
+                  black: "#000000",
+                  action: "#EF4F4F",
+                  gray: "#1a1a24",
+                },
+                typography: {
+                  fontFamily: "Gruppo",
+                },
+              }
+            : {
+                // palette values for dark mode
+                primary: {
+                  light: "#121212",
+                  main: "#212121",
+                  dark: "#ffffff",
+                },
+                secondary: {
+                  main: "#6261bd",
+                },
+                otherColors: {
+                  white: "#ffffff",
+                  black: "#000000",
+                  action: "#EF4F4F",
+                  gray: "#d4d4d4",
+                },
+                typography: {
+                  fontFamily: "Gruppo",
+                },
+              }),
         },
-    }), [mode]);
+      }),
+    [mode]
+  );
 
-    return <appThemeContext.Provider value={toggleMode}>
-        <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider>
+  return (
+    <appThemeContext.Provider value={toggleMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </appThemeContext.Provider>
-}
+  );
+};
 
 const useAppTheme = () => useContext(appThemeContext);
 
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,6 +7,7 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/btn" element={<BtnGeneratorPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
